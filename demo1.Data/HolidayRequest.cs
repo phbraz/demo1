967c620fbc78fa5f1315ce205a6acdd6636673a2
Paulo.Braz <paulo.braz@awsm.tech>
2021-07-02 08:57:26 +0100

    first commit

---
+++ demo1.Data/HolidayRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1.Data
+{
+    public class HolidayRequest
+    {
+        [Required]
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string RequesterName { get; set; }
+        public HolidayType HolidayType { get; set; }
+
+
+    }
+
+    public enum HolidayType
+    {
+        Sickness,
+        Maternity,
+        AnnualLeave
+    }
+}
---
+++ demo1.Data/Migrations/20210629151041_firstTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace demo1.Data.Migrations
+{
+    public partial class firstTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HolidayRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RequesterName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HolidayType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HolidayRequests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HolidayRequests");
+        }
+    }
+}
